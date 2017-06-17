33d48b0111a9d095178e80d8b043910762b96d62
cs <hasancelik@std.sehir.edu.tr>
2017-06-17 18:19:52 +0300

    rounding added to basic_fva_scaler

--- src/preprocessing/basic_fold_change_preprocessing.py
+++ src/preprocessing/basic_fold_change_preprocessing.py
@@ -8,12 +8,19 @@ class BasicFoldChangeScaler(TransformerMixin):
     '''
 
     def fit(self, X, y):
-        self.avgs_ = average_by_label(X, y, 'h')
+        self.avgs_ = average_by_label(self.round(X), y, 'h')
         return self
 
     def transform(self, X):
-        return [{k: self.scale(k, v) for k, v in x.items()} for x in X]
+        return [{k: self.scale(k, v)
+                 for k, v in x.items()} for x in self.round(X)]
 
     def scale(self, k, v):
         e = v / self.avgs_[k]
         return max(1 - e**-1, -10) if self.avgs_[k] > v else min(e - 1, 10)
+
+    def round(self, X):
+        for x in X:
+            for k, v in x.items():
+                x[k] = round(v, 3)
+        return X
