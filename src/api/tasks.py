a2f16129b08d5e1c3b7d41bea512c44993c88a15
cs <hasancelik@std.sehir.edu.tr>
2017-06-24 21:11:48 +0300

    task updated

--- src/api/tasks.py
+++ src/api/tasks.py
@@ -5,20 +5,22 @@ from preprocessing import DynamicPreprocessing
 from .app import celery
 from .models import db, Analysis
 
-pathway_scaler = DynamicPreprocessing(
-    ['pathway-scoring', 'transport-elimination'])
-
-with open('../models/api_model.p', 'rb') as f:
-    reaction_scaler = pickle.load(f)
-
 
 @celery.task()
 def save_analysis(analysis_id, concentration_changes):
-    analysis = Analysis.query.get(analysis_id)
+
+    with open('../models/api_model.p', 'rb') as f:
+        reaction_scaler = pickle.load(f)
+
+    pathway_scaler = DynamicPreprocessing(
+        ['pathway-scoring', 'transport-elimination'])
+
     results_reaction = reaction_scaler.transform(concentration_changes)
+    results_pathway = pathway_scaler.transform(results_reaction)
+
+    analysis = Analysis.query.get(analysis_id)
     analysis.results_reaction = analysis.clean_name_tag(results_reaction)
-    analysis.results_pathway = analysis.clean_name_tag(
-        pathway_scaler.transform(results_reaction))
+    analysis.results_pathway = analysis.clean_name_tag(results_pathway)
     analysis.status = True
     analysis.end_time = datetime.datetime.now()
     db.session.commit()
